97e8dc8d4ca793068ee8a52699d99562e897b77e
martin20162020 <69869010+martin20162020@users.noreply.github.com>
2021-09-17 21:01:37 -0500

    Add files via upload

---
+++ todo-redux/src/components/todos/TodoForm.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { Form, Field } from "react-final-form";
+import '../../styles/addTodo.css'
+
+
+const TodoForm = (props) => {
+  const renderError = ({ error, touched }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+
+  const renderInput = ({ input, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "errors" : ""} form-control`;
+    return (
+      <div className={className}>
+        <input {...input} className="form-control" id="title" autoComplete="off" complete='false'/>
+        {renderError(meta)}
+        {console.log('INPUT',input)}
+      </div>
+    );
+  };
+
+
+
+
+  const onSubmit = (formValues) => {
+    props.onSubmit(formValues);
+    formValues.title=''
+  };
+
+//   <Form onSubmit={handleSubmit(this.handleSubmitAction)}>
+//     <Field name="input" component={renderTitleInput} onChangeAction={this.handleInputChange} defaultValue={this.state.input} ref={(input) => this.input = input} />
+//       <input type="submit"  name="submit" />
+// </Form>
+/** HandleSubmit function **/
+
+
+  return (
+    <Form
+      initialValues={props.initialValues}
+      onSubmit={onSubmit}
+      
+      
+      validate={(formValues) => {
+        const errors = {};
+
+        if (!formValues.title) {
+          errors.title = <h3 className='errors'>You must enter a title</h3>;
+        }
+
+        return errors;
+      }}
+      
+      render={({ handleSubmit }) => (
+        <div>
+        <form onSubmit={handleSubmit} id="title">
+          <Field name="title" component={renderInput}/>
+
+          <button className="todo-button-submit">
+          <i className="fas fa-search"></i>
+            <span className="extra"> Submit </span>
+          </button>
+        </form>
+        </div>
+      )}
+    />
+  );
+};
+
+export default TodoForm;
